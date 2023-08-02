66ffca5174f81a1df9369a63c5ca7e38bf3d9a7c
Marc Giro <marc.piraces@cognativinc.com>
2023-08-02 23:43:19 +0200

    updated config and added express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+import express, { Express, Request, Response } from 'express';
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
